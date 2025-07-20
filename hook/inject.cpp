281f1a752ad8a04470551bb403204e2de929e761
ACCESS-DENIIED <100420164+ACCESS-DENIIED@users.noreply.github.com>
2025-07-20 11:01:18 +0100

    Load DLL only if -mp flag is present
    Added command line parsing to check for the -mp flag before loading DLLs. Changed DLL search directory to SMT/Scripts and updated the pattern to match all DLL files. Outputs debug message if -mp flag is not found and skips DLL loading.

--- hook/inject.cpp
+++ hook/inject.cpp
@@ -3,16 +3,38 @@
 #include <regex>
 #include "utils.h"
 #include <tchar.h>
+#include <shellapi.h>
 
 
 std::vector<HMODULE> LoadInjectDlls(HMODULE hModule) {
     TCHAR output[2048];
 
+    bool mpEnabled = false;
+    {
+        LPWSTR cmdLine = GetCommandLineW();
+        int argc;
+        LPWSTR* argv = CommandLineToArgvW(cmdLine, &argc);
+        if (argv) {
+            for (int i = 0; i < argc; ++i) {
+                if (_wcsicmp(argv[i], L"-mp") == 0) {
+                    mpEnabled = true;
+                    break;
+                }
+            }
+            LocalFree(argv);
+        }
+    }
+
+    if (!mpEnabled) {
+        OutputDebugString(L"-mp flag was not found.");
+        return {};
+    }
+
     std::filesystem::path dllPath = GetCurrentDllPath(hModule);
-    std::filesystem::path smtDir = dllPath.parent_path() / _T("SMT");
+    std::filesystem::path smtDir = dllPath.parent_path() / _T("SMT") / _T("Scripts");
     std::vector<std::filesystem::path> dllList;
 
-    std::basic_regex<TCHAR> pattern(_T("^winmm\\..+\\.dll$"));
+    std::basic_regex<TCHAR> pattern(_T(".*\\.dll$"));
     if (std::filesystem::exists(smtDir) && std::filesystem::is_directory(smtDir)) {
         for (const auto& entry : std::filesystem::directory_iterator(smtDir)) {
             if (entry.is_regular_file()){
